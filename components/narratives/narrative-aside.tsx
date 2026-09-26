543961bec25643b6eb46efafc28d4a3596f56e05
t0rn1k3 <tornikeburjanadze0@gmail.com>
2026-09-26 12:40:21 +0400

    Remove Explore Narratives button from narrative list aside

--- components/narratives/narrative-aside.tsx
+++ components/narratives/narrative-aside.tsx
@@ -1,27 +1,15 @@
 "use client";
 
-import { useCallback, useRef } from "react";
-
-import { AsideFooter } from "./aside-footer";
 import { AsideMasthead } from "./aside-masthead";
 import { NarrativeList } from "./narrative-list";
 
 export function NarrativeAside() {
-  const listRef = useRef<HTMLDivElement>(null);
-
-  const scrollToList = useCallback(() => {
-    listRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
-  }, []);
-
   return (
     <aside className="flex h-full max-h-full flex-col overflow-hidden rounded-xl border border-nara-grey-300/25 bg-[var(--nara-aside-bg)] text-black/80 shadow-[0_8px_32px_rgba(11,20,38,0.12)] backdrop-blur-sm">
       <AsideMasthead />
       <div className="aside-scroll min-h-0 flex-1 overflow-y-auto overscroll-contain">
-        <div ref={listRef}>
-          <NarrativeList />
-        </div>
+        <NarrativeList />
       </div>
-      <AsideFooter onExplore={scrollToList} />
     </aside>
   );
 }
